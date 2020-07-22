b0ca20a928c75362adf91dc9a8efa7700ed4d69a
Richard Simpson <magnum1997@gmail.com>
2020-07-22 07:28:45 -0700

    Remove unnecessary code and comments.

--- BankDefault.lua
+++ BankDefault.lua
@@ -1,20 +1,13 @@
--- First, we create a namespace for our addon by declaring a top-level table that will hold everything else.
 BankDefault = {}
-
--- This isn't strictly necessary, but we'll use this string later when registering events.
--- Better to define it in a single place rather than retyping the same string.
 
 BankDefault.name = "BankDefault"
 -- BankDefault.Name = “BankDefault”
 -- BankDefault.version = “0.0.1”
 -- BankDefault.author = “Magnum1997”
--- local 
--- Next we create a function that will initialize our addon
 
 function BankDefault:Initialize()
     d(BankDefault.name, "loaded.")
 end
-
 
 -- Then we create an event handler function which will be called when the "addon loaded" event
 -- occurs. We'll use this to initialize our addon after all of its resources are fully loaded.
@@ -48,12 +41,5 @@ function selectDeposit()
     --bankFragmentBar:SelectFragment(SI_BANK_DEPOSIT)
 end
 
-function BankDefault.OnBankClose(event, bankBag)
-    --  if bankBag ~= BANK_BAG return end
-    d("Closed bank window")
-end
-
--- Finally, we'll register our event handler function to be called when the proper event occurs.
-EVENT_MANAGER:RegisterForEvent(BankDefault.name, EVENT_ADD_ON_LOADED, BankDefault.OnAddOnLoaded)
+--EVENT_MANAGER:RegisterForEvent(BankDefault.name, EVENT_ADD_ON_LOADED, BankDefault.OnAddOnLoaded)
 EVENT_MANAGER:RegisterForEvent(BankDefault.name, EVENT_OPEN_BANK, BankDefault.OnBankOpen)
-EVENT_MANAGER:RegisterForEvent(BankDefault.name, EVENT_CLOSE_BANK, BankDefault.OnBankClose)
