e9a4779b5580a41943ab9b6dc53384cdad483a02
Richard Simpson <magnum1997@gmail.com>
2020-08-14 00:35:28 -0700

    Change name, description, version
    Lots of code and comment cleanup

--- BankDefault.lua
+++ BankDefault.lua
@@ -1,58 +1,36 @@
-BankDefault = {}
+BankSettings = {
+    name = "BankSettings", -- Matches folder and Manifest file names.
+    version = "0.2.0", -- A nuisance to match to the Manifest.
+    author = "@Magnum1997",
+    menuName = "Mag's Bank Settings", -- A UNIQUE identifier for menu object.
+}
 
-BankDefault.name = "BankDefault"
--- BankDefault.Name = “BankDefault”
--- BankDefault.version = “0.0.1”
--- BankDefault.author = “Magnum1997”
+function BankSettings.OnAddOnLoaded(event, addonName)
+    -- The event fires each time *any* addon loads - but we only care about when our own addon loads.
+    if addonName == BankSettings.name then
+        EVENT_MANAGER:UnregisterForEvent("BankSettings", EVENT_ADD_ON_LOADED)
+    end
+    BankSettings:Initialize()
+end
 
-function BankDefault:Initialize()
-    d(BankDefault.name, "loaded.")
+function BankSettings:Initialize()
     local bankScene = SCENE_MANAGER:GetScene("bank")
     bankScene:RegisterCallback("StateChange", function(oldState, newState)
-        if newState == SCENE_SHOWING then
-            BankDefault.selectDeposit()
+        if newState == SCENE_SHOWN then
+            BankSettings.selectDeposit()
         end
     end)
 end
 
--- Then we create an event handler function which will be called when the "addon loaded" event
--- occurs. We'll use this to initialize our addon after all of its resources are fully loaded.
-
-function BankDefault.OnAddOnLoaded(event, addonName)
-    -- The event fires each time *any* addon loads - but we only care about when our own addon loads.
-    if addonName == BankDefault.name then
-        EVENT_MANAGER:UnregisterForEvent("BankDefault", EVENT_ADD_ON_LOADED)
-        BankDefault:Initialize()
+function BankSettings.selectDeposit(bankBag)
+    if (IsBankOpen and IsHouseBankBag(bankBag)) then
+        d("Bag is house bank")
+        ZO_MenuBar_SelectDescriptor(ZO_HouseBankMenuBar, SI_BANK_DEPOSIT, true, true)
+    else
+        d("Bag is normal bank")
+        ZO_MenuBar_SelectDescriptor(ZO_PlayerBankMenuBar, SI_BANK_DEPOSIT, true, true)
     end
 end
 
-function BankDefault.OnBankOpen(event, bankBag)
-    --Do not call the scene callback function registration with EACH bank open!
-    --It's enopugh to do this once after addon init.
-end
-
---Better do not name a function just selectDeposit as noone knows where it belongs to, and
---it polluates the global namespace (table _G["selectDeposit"]) -> same like selectDeposit)
---So move it to your addon's namespace (a table for your addon -> BankDefault)
-function BankDefault.selectDeposit()
-    -- Two method that work shown below. When run with debug or /script command
-    -- with the bank window open it works perfectly.
-    -- Run in callback the item categories misalign.
-    -- Can be fixed by clicking withdraw button.
-
-    ZO_MenuBar_SelectDescriptor(ZO_PlayerBankMenuBar, SI_BANK_DEPOSIT)
-
-    --local bankFragmentBar = ZO_SceneFragmentBar:New(ZO_PlayerBankMenuBar)
-    --bankFragmentBar:SelectFragment(SI_BANK_DEPOSIT)
-    --> Do not manually mess with the fragments as this will most likely break other addons!!!
-    --> Like LibFilters-3.0 which adds stuff to the fragment's layout to identify what is currently shown.
-    --> There also exist functions in the SCENE_MANAGER to add/remove fragments to a scene, 
-    --> which are called as the bank switches between deposit and withdraw.
-    --> e.g. removes inventory fragment from bank scene as you switch from despoit to withdraw and adds the bank fragment
-    -->  + vice versa
-    --It should be enough to use ZO_MenuBar_SelectDescriptor to select the menubar's button, which will call the
-    --scene change events and functions automatically afterwards.
-end
-
---EVENT_MANAGER:RegisterForEvent(BankDefault.name, EVENT_ADD_ON_LOADED, BankDefault.OnAddOnLoaded)
-EVENT_MANAGER:RegisterForEvent(BankDefault.name, EVENT_OPEN_BANK, BankDefault.OnBankOpen)
+-- When any addon is loaded, but before UI (Chat) is loaded.
+EVENT_MANAGER:RegisterForEvent(BankSettings.name, EVENT_ADD_ON_LOADED, BankSettings.OnAddOnLoaded)
